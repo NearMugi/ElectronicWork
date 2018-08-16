d45a573e8b2222db7fe7bb0c0d17d27d1c229274
Teppei Kuroda <niamugi@gmail.com>
2018-08-16 11:54:12 +0900

    loopSample追加

---
+++ loopSample/loopSample.ino
@@ -0,0 +1,42 @@
+#include "interval.h"
+
+#define PIN_1 3
+#define PIN_2 4
+#define PIN_3 5
+bool sw_1;
+bool sw_2;
+bool sw_3;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(PIN_1, OUTPUT);
+  pinMode(PIN_2, OUTPUT);
+  pinMode(PIN_3, OUTPUT);
+  sw_1 = false;
+  sw_2 = false;
+  sw_3 = false;
+
+  digitalWrite(PIN_1, sw_1);
+  digitalWrite(PIN_2, sw_2);
+  digitalWrite(PIN_3, sw_3);
+
+}
+
+void loop() {
+
+  interval<10000>::run([] {
+    sw_1 = !sw_1;
+    digitalWrite(PIN_1, sw_1);
+  });
+
+  interval<250000>::run([] {
+    sw_2 = !sw_2;
+    digitalWrite(PIN_2, sw_2);
+  });
+
+  interval<1000000>::run([] {
+    sw_3 = !sw_3;
+    digitalWrite(PIN_3, sw_3);
+  });
+
+}
