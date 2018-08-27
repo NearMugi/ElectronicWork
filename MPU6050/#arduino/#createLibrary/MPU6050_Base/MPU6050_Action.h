48145c88e57e9efd881e778277cd843e7255e574
Teppei Kuroda <niamugi@gmail.com>
2018-08-27 17:33:53 +0900

    mpu6050でアクションを判定する処理を追加。 動作確認済み。閾値は今後調整あるかも。

---
+++ MPU6050/#arduino/#createLibrary/MPU6050_Base/MPU6050_Action.h
@@ -0,0 +1,181 @@
+#ifndef MPU6050_ACTION_H
+#define MPU6050_ACTION_H
+#include <Arduino.h>
+
+//上面(1～6)
+#define Z_P 1
+#define Z_M 6
+#define X_P 2
+#define X_M 5
+#define Y_P 3
+#define Y_M 4
+
+//アクション
+#define ACT_KEEP 0    //変化なし
+#define ACT_SHAKE_V 1 //垂直方向に振る
+#define ACT_SHAKE_H 2 //水平方向に降る
+#define ACT_KNOCK 3   //ノックする
+
+#define MAX_IDX 100
+
+
+class MPU6050_Action {
+  public:
+    int top;
+    int action;
+
+    void initJdgAct() {
+      top = Z_P;
+      action = ACT_KEEP;
+      idx = 0;
+      accel_max[0] = 0;
+      accel_max[1] = 0;
+      accel_max[2] = 0;
+      for (int i = 0; i < MAX_IDX; i++) {
+        accel_abs[0][i] = 0;
+        accel_abs[1][i] = 0;
+        accel_abs[2][i] = 0;
+      }
+    };
+
+    void Jdg_Top(float _gravity[3]) {
+      //絶対値が一番大きい座標を上の面とする。
+      //その座標の２面のうちどちらが上になっているかは正負で判断する。
+      int abs_x = _gravity[0] * (float)100;
+      abs_x = abs(abs_x);
+      int abs_y = _gravity[1] * (float)100;
+      abs_y = abs(abs_y);
+      int abs_z = _gravity[2] * (float)100;
+      abs_z = abs(abs_z);
+
+      if (abs_x > abs_y && abs_x > abs_z) {
+        if (_gravity[0] >= 0) {
+          top = X_P;
+        } else {
+          top = X_M;
+        }
+      }
+
+      if (abs_y > abs_x && abs_y > abs_z) {
+        if (_gravity[1] >= 0) {
+          top = Y_P;
+        } else {
+          top = Y_M;
+        }
+      }
+
+      if (abs_z >= abs_x && abs_z >= abs_y) {
+        if (_gravity[2] >= 0) {
+          top = Z_P;
+        } else {
+          top = Z_M;
+        }
+      }
+    };
+
+#define BORDER_LINE_SHAKE 2500  //一度でもこの値を超えたとき"振った"と判断する
+#define BORDER_LINE_BOTTOM 200  //山の下限(下がったところ)
+#define MOUNTAIN_CNT 4  //振ったと判断する山の数
+#define BORDER_LINE_KEEP 100    //この値より小さいときは"維持かも"と判断する
+#define KEEP_CNT 10 //BORDER_LINE_KEEPをKEEP_CNT回連続して下回ると"維持"と判断する
+    void Jdg_Action(int _worldAccel[3]) {
+      //加速度の変化量を基に振っているかどうか判断する。
+      action = ACT_KEEP;
+
+      //加速度の絶対値と最大値を保存
+      for (int i = 0; i < 3; i++) {
+        accel_max[i] = 0;
+        accel_abs[i][MAX_IDX] = abs(_worldAccel[i]);  //配列の最後に入れる
+        for (int j = 0; j < MAX_IDX; j++) {
+          //前に詰める
+          accel_abs[i][j] = accel_abs[i][j + 1];          
+          if (accel_abs[i][j] > accel_max[i]) accel_max[i] = accel_abs[i][j];          
+        }
+      }
+
+      //次のループに向けてインクリメント
+      if (++idx >= MAX_IDX) {
+        idx = MAX_IDX - 1;
+      }
+
+      //解析
+      //連続した山の数が指定個以上
+      //一度でもボーダーラインを超える
+      //3軸の中で一番高い山
+      //上記を満たす場合、その方向に振ったと判断する。
+      bool isclear[3];//条件を満たした。
+      int mountain[3]; //山の数
+      int tmp_max[3];  //山の中で一番高い値
+      int keep_cnt; //連続して下回った回数
+      bool isJdgStart; //山かも
+
+      for (int i = 0; i < 3; i++) {
+        isclear[i] = false;
+        mountain[i] = 0;
+        tmp_max[i] = 0;
+        //一度もボーダーラインを超えていない場合は何もしない。
+        if (accel_max[i] < BORDER_LINE_SHAKE) {
+          continue;
+        }
+
+        isJdgStart = false;
+        keep_cnt = 0;
+        //山を数える
+        for (int j = 0; j < idx; j++) {
+          if (isclear[i]) break;
+
+          if (!isJdgStart) {
+            if (accel_abs[i][j] > BORDER_LINE_KEEP) {
+              isJdgStart = true;
+              keep_cnt = 0;
+            } else {
+              keep_cnt++;
+              //"維持"が一定以上続いた場合は全てリセット
+              if (keep_cnt >= KEEP_CNT) {
+                isJdgStart = false;
+                keep_cnt = KEEP_CNT;
+                mountain[i] = 0;
+                tmp_max[i] = 0;
+              }
+            }
+          }
+
+          if (isJdgStart) {
+            if (accel_abs[i][j] > tmp_max[i]) tmp_max[i] = accel_abs[i][j];
+            //下限に達したとき、山であると判断する
+            if (accel_abs[i][j] < BORDER_LINE_BOTTOM) {
+              mountain[i]++;
+              isJdgStart = false; //次の山を見つける
+
+              if (mountain[i] >= MOUNTAIN_CNT) isclear[i] = true;
+            }
+          }
+
+        }
+      }
+
+      //3軸を比較してアクションを判定する
+      //x,y方向はどちらも"横振り"と判断するので、xまたはyとzを比較する
+      bool isHorizon = false;
+      if (isclear[0] || isclear[1]) {
+        action = ACT_SHAKE_H;
+        isHorizon = true;
+      }
+      if (isclear[2]) {
+        if (isHorizon) {
+          if (tmp_max[2] > tmp_max[0] && tmp_max[2] > tmp_max[1]) {
+            action = ACT_SHAKE_V;
+          }
+        } else {
+          action = ACT_SHAKE_V;
+        }
+      }
+    };
+
+  private:
+    int idx;
+    int accel_max[3]; //区間内での最大値
+    int accel_abs[3][MAX_IDX + 1]; //加速度(絶対値)
+};
+
+#endif
--- MPU6050/#arduino/#createLibrary/MPU6050_Base/MPU6050_Base.ino
+++ MPU6050/#arduino/#createLibrary/MPU6050_Base/MPU6050_Base.ino
@@ -2,11 +2,27 @@
 //GND -> GND
 //SCL -> A5
 //SDA -> A4
+
+//#define SAMPLE
+#define ACTION//アクションの判定有無
+
 #include "MPU6050_Manage.h"
 MPU6050_Manage mpu_main;
 
+#ifdef ACTION
+#include "MPU6050_Action.h"
+MPU6050_Action mpu_action;
+String msg;
+String tmpSendData = "[@1,@2]";
+#define WAIT_NEXTTIME_MS 5000 //判定してからの待ち時間(ms)
+uint16_t waitTime = 0; //待ち時間
+bool isAct = false;
+#define MSG_ACCEPT "Accepting actions..."
+#define MSG_WAIT "Wait Next Time..."
+#endif
+
 //Calibration ON/OFF
-bool isCalibration;
+bool isCalibration = false;
 
 //MPU6050の初期化時に使用するオフセット
 //CalibrationがOFFの時に適用される
@@ -20,6 +36,7 @@ int mpu6050_RealAccel[3];     //[x,y,z]
 int mpu6050_WorldAccel[3];    //[x,y,z]
 uint8_t mpu6050_teapotPacket[14];
 
+
 //ループ周期(us)
 #include "interval.h"
 #define LOOP_TIME_US 10000
@@ -28,8 +45,13 @@ void setup() {
   Serial.begin(115200);
 
   //キャリブレーションする必要ない場合はオフセットを渡す
-  isCalibration = false;
   mpu_main.init(isCalibration, CalOfs);
+
+#ifdef ACTION
+  //上面・アクションの判定用変数の初期化
+  mpu_action.initJdgAct();
+  Serial.println(MSG_ACCEPT);
+#endif
 
 }
 
@@ -44,13 +66,52 @@ void loop() {
     mpu_main.Get_WorldAccel(mpu6050_WorldAccel);
     mpu_main.Get_teapotPacket(mpu6050_teapotPacket);
 
+#ifdef ACTION
+    if (isAct) {
+      //次のPublish送信が有効になるまで待ち
+      if ((millis() - waitTime) > WAIT_NEXTTIME_MS) {
+        //再開
+        mpu_action.initJdgAct();
+        Serial.println(MSG_ACCEPT);
+        isAct = false;
+      } else {
+        return;
+      }
+    }
+    mpu_action.Jdg_Top(mpu6050_Gravity);  //どの面が上になっているか判断
+    mpu_action.Jdg_Action(mpu6050_WorldAccel); //アクションを判断
+    //メッセージの作成
+    msg = tmpSendData;
+    msg.replace("@1", String(mpu_action.top));
+    msg.replace("@2", String(mpu_action.action));
+    //Serial.println(msg);
+
+    //アクションがあった時、次の判定を待つ
+    if (mpu_action.action != ACT_KEEP) {
+      switch (mpu_action.action) {
+        case ACT_SHAKE_V:
+          Serial.println("SHAKE_V");
+          break;
+        case ACT_SHAKE_H:
+          Serial.println("SHAKE_H");
+          break;
+      }
+      Serial.println(MSG_WAIT);
+      isAct = true;
+      waitTime = millis();
+    }
+
+#endif
+
+#ifdef SAMPLE
     String _s = "";
-    _s += String(mpu6050_Gravity[0]);
+    _s += String(mpu6050_WorldAccel[0]);
     _s += ",";
-    _s += String(mpu6050_Gravity[1]);
+    _s += String(mpu6050_WorldAccel[1]);
     _s += ",";
-    _s += String(mpu6050_Gravity[2]);
+    _s += String(mpu6050_WorldAccel[2]);
     Serial.println(_s);
+#endif
   });
 
 }
