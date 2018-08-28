79dae9725aec07acd4556029577a0d7ad0b4d94d
Teppei Kuroda <niamugi@gmail.com>
2018-08-28 13:42:31 +0900

    mbedフォルダ削除。BLE追加。 BLE Nano2のLチカ追加。 nrf51822をArduinoIDEでコンパイルするブートローダーを追加。(未確認)

---
+++ BLE/BLENano2_Ltika/BLENano2_Ltika.ino
@@ -0,0 +1,13 @@
+// BLE Nano2 Ltika
+#define PIN_LED P0_11
+
+void setup() {
+  pinMode(PIN_LED, OUTPUT);
+}
+ 
+void loop() {
+  digitalWrite(PIN_LED, HIGH);
+  delay(1000);
+  digitalWrite(PIN_LED, LOW);
+  delay(1000);
+}
