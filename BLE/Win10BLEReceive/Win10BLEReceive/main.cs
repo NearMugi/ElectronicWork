d450367c8cead5fa8f5f6bd10d43b7086b7a33c8
Teppei Kuroda <niamugi@gmail.com>
2018-09-02 18:01:32 +0900

    windows10 でBLE受信を追加。

---
+++ BLE/Win10BLEReceive/Win10BLEReceive/main.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Windows.Devices.Bluetooth.Advertisement;
+using Windows.Storage.Streams;
+
+namespace Win10BLEReceive
+{
+    class main
+    {
+        private void OnAdvertisementReceived(BluetoothLEAdvertisementWatcher watcher, BluetoothLEAdvertisementReceivedEventArgs eventArgs)
+        {
+            //プロパティ値から取得
+            DateTimeOffset timestamp = eventArgs.Timestamp;
+            BluetoothLEAdvertisementType advertisementType = eventArgs.AdvertisementType;
+
+            iBeacon bcon = new iBeacon(eventArgs);
+
+            string retBeaconData;
+            retBeaconData = "{";
+            retBeaconData += string.Format("uuid:'{0}',", bcon.UUID);//"00000000-0000-0000-0000-000000000000"
+            retBeaconData += string.Format("major:{0},", bcon.Major.ToString("D"));
+            retBeaconData += string.Format("minor:{0},", bcon.Minor.ToString("D"));
+            retBeaconData += string.Format("measuredPower:{0},", bcon.MeasuredPower.ToString("D"));
+            retBeaconData += string.Format("rssi:{0},", bcon.Rssi.ToString("D"));
+            retBeaconData += string.Format("accuracy:{0},", bcon.Accuracy.ToString("F6"));
+            retBeaconData += string.Format("proximity:'{0}'", bcon.Proximity);
+            retBeaconData += "}";
+
+            Console.WriteLine(string.Format("timestamp:{0}", timestamp.ToString("HH\\:mm\\:ss\\.fff")));
+            Console.WriteLine(retBeaconData);
+        }
+    }
+}
---
+++ BLE/Win10BLEReceive/Win10BLEReceive/winBeacon.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Windows.Devices.Bluetooth.Advertisement;
+using Windows.Storage.Streams;
+
+namespace Win10BLEReceive
+{
+    public class winBeacon
+    {
+        private static BluetoothLEAdvertisementWatcher watcher;
+        static void Main()
+        {
+            watcher = new BluetoothLEAdvertisementWatcher();
+            watcher.Received += OnAdvertisementReceived;//ここのイベント内で取得する
+            watcher.Start();
+            Console.WriteLine("終了するには何かキーを押してください．．．");
+            Console.ReadKey();
+            watcher.Stop();
+        }
+
+        private static void OnAdvertisementReceived(BluetoothLEAdvertisementWatcher watcher, BluetoothLEAdvertisementReceivedEventArgs eventArgs)
+        {
+            //イベントの値から取得
+            DateTimeOffset timestamp = eventArgs.Timestamp;
+            BluetoothLEAdvertisementType advertisementType = eventArgs.AdvertisementType;
+            short rssi = eventArgs.RawSignalStrengthInDBm;
+            string name = eventArgs.Advertisement.LocalName;
+
+            Console.WriteLine(string.Format("timestamp:{0}", timestamp.ToString("HH\\:mm\\:ss\\.fff")));
+            Console.WriteLine(string.Format("rssi:{0}", rssi.ToString("D")));
+        }
+    }
+
+}
