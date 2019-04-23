0c9e8faff829421f9a9dff2055f9966aa76b3e14
Teppei Kuroda <niamugi@gmail.com>
2019-04-23 14:52:17 +0900

    ATtinyで赤外線受信

--- ATtiny/ATtiny13AIRSensor/ATtiny13AIRSensor.ino
+++ ATtiny/ATtiny13AIRSensor/ATtiny13AIRSensor.ino
@@ -1,102 +1,87 @@
 // ATtiny13Aで赤外線リモコン受信
-//
-// ATMEL ATTINY13 / ARDUINO
 // 利用パッケージ：https://github.com/MCUdude/MicroCore
 // 注意： core_settings.h の#ENABLE_MICROS を有効にする必要あり
 //
 //                 +-Ｖ-+
 // Reset      PB5 1|    |8 Vcc
-// Tx         PB3 2|    |7 PB2 IR(IN)
-// Rx         PB4 3|    |6 PB1
+// Tx,Rx      PB3 2|    |7 PB2 IR(IN)
+//            PB4 3|    |6 PB1
 //            GND 4|    |5 PB0
 //                 +----+
 //
+//赤外線リモコンの通信フォーマットは以下を参考
+//http://elm-chan.org/docs/ir_format.html
 
 // シリアル通信定義
 #define BAUD_RATE 38400
-#include "BasicSerial3.h"
-#define Tx      PB3 // (変更する場合、BasicSerial3.S修正が必要)
-#define Rx      PB4 // (変更する場合、BasicSerial3.S修正が必要)
+#include <BasicSerial3.h>
 
 //IR受信定義
 #define IR            PB2
 #define IR_DDR        DDRB
 #define IR_PORT       PORTB
 #define IR_PININ      PINB
-#define IRbitRead()   (IR_PININ&_BV(IR))
+#define IRbitRead()   (IR_PININ&_BV(IR))   //※受信データはH/L反転で読まれる
 #define RC_RDH_TS     9000    // リーダコードOFF間隔  9ms判定用
 #define RC_RDL_TS     3800    // リーダコードON間隔   4.5ms判定用
 #define RC_BITLOW_TS  1000    // ビットデータON間隔   1.69ms判定用  
 #define RC_TMOVER     8000    // タイムオバー
 
-//
-// 赤外線リモコンコード取得
-// 4バイトのデータを返す
-// CCCCDDdd
-//    CCCC カスタムコード
-//    DD   データコード
-//    dd   データコードのビット反転(データチェック用)
-//　ただし、
-//    リピートコードの場合  0
-//    エラーの場合          0xFFFFFFFF
-//  を返す.
-//
+#define READ_ERR 0xFFFFFFFF
+#define READ_REPEAT 0xFFFF0000
+
+uint32_t rc;
+uint16_t custmerCode;
+uint16_t readData;
+
 uint32_t Read_IR() {
-  uint8_t  repeat = 0;  // リピートコード検出フラグ
-  uint32_t  dt    = 0;  // 赤外線リモコン読み取りデータ
   unsigned long t ;     // 信号長計測用
 
   // リード部の取得
-  // 受信データはH/L反転で読まれる
-  while(1) {
-    while(IRbitRead());  // OFF検出受信待ち   
-    t = micros();        // OFF検出時刻取得
-    while(!IRbitRead()); // ON受信検出待ち 
-    t = micros() -t;     // OFF->ONの時間間隔取得
-    if (t > RC_RDH_TS) { // 9ms以上ならリーダコードとみなす
-        t = micros();      // ON検出時刻取得
-        while(IRbitRead());// OFF検出待ち
-        t = micros() -t;   // ON->OFF時間間隔取得
-        break;
-    }
+  while (IRbitRead()); // OFF検出受信待ち
+  t = micros();        // OFF検出時刻取得
+  while (!IRbitRead()); // ON受信検出待ち
+  t = micros() - t;    // OFF->ONの時間間隔取得
+
+  // 9ms近辺ならリーダコードとみなす
+  if (t < RC_RDH_TS - 562) return READ_ERR;
+  if (t > RC_RDH_TS + 562) return READ_ERR;
+
+  t = micros();      // ON検出時刻取得
+  while (IRbitRead()); // OFF検出待ち
+  t = micros() - t;  // ON->OFF時間間隔取得
+
+  //Repeatの場合、処理を抜ける
+  //(閾値は曖昧にしている)
+  if (t < RC_RDL_TS) return READ_REPEAT;
+
+  //データを取得する
+  uint32_t dt = 0;  // 赤外線リモコン読み取りデータ
+  for (uint8_t i = 0; i < 32; i++) { //32ビット分取得ループ
+    // ビット開始待ち
+    while (!IRbitRead()); // ON待ち
+    t = micros();
+    while (IRbitRead()); // OFF待ち
+    t = micros() - t;
+    if (t > RC_TMOVER)
+      return READ_ERR;  // エラー
+    dt <<= 1;
+    dt |= (t > RC_BITLOW_TS) ? 1 : 0;
   }
-  
-  // データ部取得
-  if (t < RC_RDL_TS) {
-    // 0N->OFF がリピートコードの場合、データ取得はスキップ
-    repeat = 1;          
-  } else {
-    // 0N->OFF がリダーコードの場合、データを取得
-     for (uint8_t i = 0; i <32; i++) {  //32ビット分取得ループ
-        // ビット開始待ち
-        while(!IRbitRead());  // ON待ち
-        t = micros();
-        while(IRbitRead());  // OFF待ち
-        t = micros() -t;
-        if (t>RC_TMOVER)
-          return 0xFFFFFFFF;  // エラー
-        dt<<=1;
-        dt |= (t>RC_BITLOW_TS) ? 1:0;     
-    }
-  }
-  // ストップビットの待ち
-  while(IRbitRead());  // OFF待ち  
-  if (repeat)
-    return 0;
   return dt;
 }
 
 // 文字列出力
 void serOut(const char* str) {
-   while (*str) TxByte (*str++);
+  while (*str) TxByte (*str++);
 }
 
 // 整数を16進数で出力
 void serOutHex(uint16_t h) {
   uint8_t c;
-  for (int8_t i=12; i>=0; i-=4) {
-    c = h>>i & 0xf; 
-    c = c >9 ? c+'A'-10: c+'0';
+  for (int8_t i = 12; i >= 0; i -= 4) {
+    c = h >> i & 0xf;
+    c = c > 9 ? c + 'A' - 10 : c + '0';
     TxByte ((uint8_t)c);
   }
 }
@@ -106,13 +91,15 @@ void setup() {
 }
 
 void loop() {
-  uint32_t rc =  Read_IR();  // IR受信
-  if (rc) {
-    if (rc != 0xFFFFFFFF) {
-      serOut("custom="); serOutHex(rc>>16);serOut(" ");
-      serOut("data=");serOutHex(rc>>8&0xff);serOut("\n\r");
-    }
-  } else {
-    //serOut("Repeat\n\r");
+  rc =  Read_IR();  // IR受信
+  if (rc == READ_ERR) return;
+
+  if (rc != READ_REPEAT) {
+    custmerCode = rc >> 16;
+    readData = rc >> 8 & 0xff;
   }
+  serOutHex(custmerCode);
+  serOut(" ");
+  serOutHex(readData);
+  serOut("\n\r");
 }
